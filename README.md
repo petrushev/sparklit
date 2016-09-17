10ba7e1dc11e3c0742c8db03ee03ace4890a7804
Baze Petrushev <b.petrushev@gmail.com>
2016-09-17 20:54:34 +0200

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # sparklit
 Natural Language Processing with Python and Spark
+
+## api
+
+  * [sentenceSplit](#sentencesplit)
+  * [tokenizeSentences](#tokenizesentences)
+  * [ginSentences](#ginsentences)
+  * [search](#search)
+  * [vocabulary](#vocabulary)
+  * [tf](#tf), [idf](#idf), [tfidf](#tfidf)
+
+
+### sentenceSplit
+    Takes raw text rdd and transforms it into rdd containing sentences only
+
+
+### tokenizeSentences
+    Takes sentences only rdd and transforms it into rdd of key-value pairs,
+    the key being the sentence, and the value being list of tokens
+
+
+### ginSentences
+    Takes sentences only rdd and transforms it into gin index rdd,
+    the key is a word and the value is an iterable of sentences
+
+
+### search
+    Takes gin rdd and returns rdd of sentences that contain the token
+
+
+### vocabulary
+    Takes sentence-token list rdd and transforms it into the vocabulary rdd
+    containing only the alphabetical tokens from the text
+
+
+### tf
+    Build tf breakdown of sentence-token list. 
+    The resulting key is sentence-token pair.
+
+
+### idf
+    Build idf breakdown of sentence-token list.
+    The resulting key is sentence.
+
+
+### tfidf
+    Build tf-idf breakdown of sentence-token list.
+    The resulting key is sentence-token.
+
+
+## Running tests
+
+You will need **[nose](nose.readthedocs.io)** to run tests:
+
+   ``nosetests2 test.py``
--- sparklit/__init__.py
+++ sparklit/__init__.py
@@ -14,35 +14,71 @@ englishPunktTokenizer = nltkDataload('tokenizers/punkt/english.pickle')
 wordPunktTokenizer = WordPunctTokenizer()
 
 def sentenceSplit(textRdd, sentTokenizer=englishPunktTokenizer):
+    """
+    Takes raw text rdd and transforms it into rdd containing sentences only
+    @param sentTokenizer: object with method `tokenize`
+    """
     return textRdd.flatMap(englishPunktTokenizer.tokenize)\
                   .map(string.strip).filter(lambda sent: sent != '')
 
 def tokenizeSentences(sentenceRdd, wordTokenizer=wordPunktTokenizer):
+    """
+    Takes sentences only rdd and transforms it into rdd of key-value pairs,
+    the key being the sentence, and the value being list of tokens
+    @param wordTokenizer: object with method `tokenize`
+    """
     return sentenceRdd.map(lambda sent: (sent, wordTokenizer.tokenize(sent)))
 
 def _filterTokens(tokens):
+    """
+    Filter out non-alphabetical tokens and put the rest of them in lowercase
+    """
     return [token.lower() for token in tokens
             if token.isalpha()]
 
 def ginSentences(sentenceRdd, wordTokenizer=wordPunktTokenizer):
+    """
+    Takes sentences only rdd and transforms it into gin index rdd,
+    the key is a word and the value is an iterable of sentences
+    @param wordTokenizer: object with method `tokenize`
+    """
     return tokenizeSentences(sentenceRdd, wordTokenizer)\
-               .flatMap(lambda (sent, tokens): \
-                            [(token, sent) for token in set(_filterTokens(tokens))])\
-               .groupByKey()
+        .flatMap(lambda (sent, tokens): \
+                 [(token, sent) for token in set(_filterTokens(tokens))])\
+        .groupByKey()
 
 def search(gin, token):
+    """
+    Takes gin rdd and returns rdd of sentences that contain the token
+    """
     return gin.filter(lambda (_token, sents): _token == token)\
               .flatMap(lambda (_token, sents): sents)
 
 def vocabulary(sentWordMapRdd):
+    """
+    Takes sentence-token list rdd and transforms it into the vocabulary rdd
+    containing only the alphabetical tokens from the text
+    """
     return sentWordMapRdd.flatMap(lambda (sent, tokens): _filterTokens(tokens))\
                .map(lambda token: (token, None))\
                .groupByKey().keys()
 
+def _tf(tokens):
+    freq = {}
+    tokens_ = _filterTokens(tokens)
+    for token in tokens_:
+        freq[token] = freq.get(token, 0) + 1
+    total = len(tokens_)
+    tf = [(token, cnt * 1.0 / total)
+          for token, cnt in freq.iteritems()]
+    return tf
+
 def tf(sentWordMapRdd):
-    return sentWordMapRdd.flatMap(lambda (sent, tokens): \
-                                      [((sent, token), 1) for token in _filterTokens(tokens)])\
-                         .foldByKey(0, add)
+    """Build tf breakdown of sentence-token list.
+    The resulting key is sentence-token pair.
+    """
+    return sentWordMapRdd.flatMap(
+        lambda (sent, tokens): [((sent, token), tf_) for token, tf_ in _tf(tokens)])
 
 @contextmanager
 def temporaryPersisted(rdd, storageLevel=StorageLevel.DISK_ONLY):
@@ -59,6 +95,9 @@ def temporaryPersisted(rdd, storageLevel=StorageLevel.DISK_ONLY):
         rdd.unpersist()
 
 def idf(sentWordMapRdd):
+    """Build idf breakdown of sentence-token list.
+    The resulting key is sentence
+    """
     with temporaryPersisted(sentWordMapRdd) as sentWordMapRdd:
         vocabularySize = vocabulary(sentWordMapRdd).count()
         df = sentWordMapRdd.flatMap(lambda (sent, tokens): \
@@ -69,6 +108,9 @@ def idf(sentWordMapRdd):
     return res
 
 def tfidf(sentWordMapRdd):
+    """Build tf-idf breakdown of sentence-token list.
+    The resulting key is sentence-token
+    """
     with temporaryPersisted(sentWordMapRdd) as sentWordMapRdd:
         tfSent = tf(sentWordMapRdd).map(lambda ((sent, token), val): (token, (sent, val)))
         res = tfSent.join(idf(sentWordMapRdd))\
